55c4d0a02996111a0a593a8cac3b2e13522e9e0d
MaDeBacBIT <maarten.de.backere@student.howest.be>
2026-05-13 15:30:23 +0200

    Loops for UMAP/FeaturePlot and add viridis colors
    Replaced repetitive UMAP and FeaturePlot code with loops, started file path management with base_dir, added viridis package for color scale in DotPlot and FeaturePlot, improved axis labels of DotPlot

--- scripts/analysis_base_RNA_MDB.R
+++ scripts/analysis_base_RNA_MDB.R
@@ -6,7 +6,7 @@
 #######################################
 # Check if the required packages are installed - if not, install them
 
-req_packages <- c("Seurat","ggplot2","harmony","openxlsx")
+req_packages <- c("Seurat","ggplot2","harmony","openxlsx","viridis")
 
 my_packages <- installed.packages()[, "Package"] #list installed packages
 for (pkg in req_packages) {
@@ -21,11 +21,17 @@ library(ggplot2)
 library(harmony)
 library(readxl)
 library(openxlsx)
+library(viridis)
 set.seed(1234)
 
 #######################################
 # load in the preprocessed data
 #######################################
+
+# store location where the data, figures and scripts are located, without / on the end
+base_dir <- "/scratch/svc_td_compbio/users/MaDeBa"
+
+
 # load("/scratch/svc_td_compbio/users/MaDeBa/data/seurat_objects_list_RNAonly_filtered_merged_normalized_HVG_joined.RData")
 
 # have a look at the seurat object
@@ -47,8 +53,8 @@ load("/scratch/svc_td_compbio/users/MaDeBa/data/seurat_objects_list_RNAonly_filt
 
 # seurat_obj_merged_joined@assays
 # Assays(seurat_obj_merged_joined)
-VariableFeatures(seurat_obj_merged_joined)[1:20]
-DefaultAssay(seurat_obj_merged_joined)
+# VariableFeatures(seurat_obj_merged_joined)[1:20]
+# DefaultAssay(seurat_obj_merged_joined)
 
 # create patient column with patient id, extracted from orig.ident
 # seurat_obj_merged_joined@meta.data$patient <- sub(".*-(Patient[0-9]+).*", "\\1", seurat_obj_merged_joined@meta.data$orig.ident)
@@ -146,6 +152,7 @@ dev.off()
 
 # Integration will be located here in the analysis
 
+
 # -------------------------- build in save method here -----------------
 
 #######################################
@@ -168,15 +175,14 @@ seurat_obj_merged_joined <- FindClusters(seurat_obj_merged_joined, resolution = 
 # UMAP Visualization
 #######################################
 
-# Run umap, with PCA dimensions 1:30
+# Run umap reduction method, with PCA dimensions 1:30
 # seurat_obj_merged_joined <- RunUMAP(seurat_obj_merged_joined, dims = 1:30)
 seurat_obj_merged_joined <- RunUMAP(seurat_obj_merged_joined, dims = 1:27)
 
-
 # Visualize clusters (to pdf file)
-pdf(file = "/scratch/svc_td_compbio/users/MaDeBa/figures/explore_seurat/rna_umap_v1.pdf", width=12, height=9)
-DimPlot(seurat_obj_merged_joined, reduction = "umap")
-dev.off()
+# pdf(file = "/scratch/svc_td_compbio/users/MaDeBa/figures/explore_seurat/rna_umap_v1.pdf", width=12, height=9)
+# DimPlot(seurat_obj_merged_joined, reduction = "umap")
+# dev.off()
 
 # check possible groupings on according to metadata
 # colnames(seurat_obj_merged_joined[[]])
@@ -185,97 +191,152 @@ dev.off()
 # DimPlot(seurat_obj_merged_joined, reduction = "umap", group.by = "orig.ident") + 
 #   NoLegend()
 
-# Visualize clusters (to pdf file) grouped by cluster, with label and higher resolution
-pdf(file = "/scratch/svc_td_compbio/users/MaDeBa/figures/explore_seurat/rna_umap_1-27dim_06res_cluster.pdf",
-    width=12,
-    height=9)
-DimPlot(seurat_obj_merged_joined,
-        reduction = "umap",
-        group.by = "seurat_clusters",
-        label = TRUE,
-        raster.dpi = c(600,600)) + 
-  NoLegend() +
-  labs(title = "RNA - cluster (PCA dims = 1:27, res = 0.6)") +
-  theme(
-    plot.title = element_text(hjust=0.5)
+# # Visualize clusters (to pdf file) grouped by cluster, with label and higher resolution
+# pdf(file = "/scratch/svc_td_compbio/users/MaDeBa/figures/explore_seurat/rna_umap_1-27dim_06res_cluster.pdf",
+#     width=12,
+#     height=9)
+# DimPlot(seurat_obj_merged_joined,
+#         reduction = "umap",
+#         group.by = "seurat_clusters",
+#         label = TRUE,
+#         raster.dpi = c(600,600)) + 
+#   NoLegend() +
+#   labs(title = "RNA - cluster (PCA dims = 1:27, res = 0.6)") +
+#   theme(
+#     plot.title = element_text(hjust=0.5)
+#   )
+# dev.off()
+# 
+# ## BY PATIENT
+# 
+# pdf(file = "/scratch/svc_td_compbio/users/MaDeBa/figures/explore_seurat/rna_umap_1-27dim_06res_patient.pdf",
+#     width=12,
+#     height=9)
+# DimPlot(seurat_obj_merged_joined,
+#         reduction = "umap",
+#         group.by = "patient",
+#         label = FALSE,
+#         raster.dpi = c(600,600)) +
+#   labs(title = "RNA - patient (PCA dims = 1:27, res = 0.6)") +
+#   theme(
+#     plot.title = element_text(hjust=0.5),
+#     legend.position = "bottom"
+#         )
+# dev.off()
+# 
+# ## BY SAMPLE
+# pdf(file = "/scratch/svc_td_compbio/users/MaDeBa/figures/explore_seurat/rna_umap_1-27dim_06res_sample.pdf",
+#     width=18,
+#     height=9)
+# DimPlot(seurat_obj_merged_joined,
+#         reduction = "umap",
+#         group.by = "orig.ident",
+#         label = FALSE,
+#         raster.dpi = c(900,900)) +
+#   labs(title = "RNA - sample (PCA dims = 1:27, res = 0.6)") +
+#   theme(
+#     plot.title = element_text(hjust=0.5),
+#     legend.position = "right"
+#   )
+# dev.off()
+# 
+# ## BY ncount
+# pdf(file = "/scratch/svc_td_compbio/users/MaDeBa/figures/explore_seurat/rna_umap_1-27dim_06res_ncount.pdf",
+#     width=12,
+#     height=9)
+# FeaturePlot(seurat_obj_merged_joined,
+#         reduction = "umap",
+#         features = "nCount_RNA") +
+#   # NoLegend() +
+#   labs(title = "RNA - nCount (PCA dims = 1:27, res = 0.6)") +
+#   theme(
+#     plot.title = element_text(hjust=0.5)
+#   )
+# dev.off()
+# 
+# ## BY nfeature
+# pdf(file = "/scratch/svc_td_compbio/users/MaDeBa/figures/explore_seurat/rna_umap_1-27dim_06res_nfeature.pdf",
+#     width=12,
+#     height=9)
+# FeaturePlot(seurat_obj_merged_joined,
+#             reduction = "umap",
+#             features = "nFeature_RNA") +
+#   # NoLegend() +
+#   labs(title = "RNA - nFeature (PCA dims = 1:27, res = 0.6)") +
+#   theme(
+#     plot.title = element_text(hjust=0.5)
+#   )
+# dev.off()
+# 
+# ## BY percentmt
+# pdf(file = "/scratch/svc_td_compbio/users/MaDeBa/figures/explore_seurat/rna_umap_1-27dim_06res_percentmt.pdf",
+#     width=12,
+#     height=9)
+# FeaturePlot(seurat_obj_merged_joined,
+#             reduction = "umap",
+#             features = "percent.mt") +
+#   # NoLegend() +
+#   labs(title = "RNA - percent.mt (PCA dims = 1:27, res = 0.6)") +
+#   theme(
+#     plot.title = element_text(hjust=0.5)
+#   )
+# dev.off()
+
+
+################################## CREATE ALL UMAP plots with 1 loop
+
+# # create subdirectory (not ending on /), chooses what to group by (also renamed) , visualize marker expression on umap, use color viridis and improve for all celltypes
+subdir_umap <- "/figures/rna_umap"
+umap_groups <- list(
+  "cluster" = "seurat_clusters",
+  "patient" = "patient",
+  "sample"= "orig.ident",
+  "nCount" = "nCount_RNA",
+  "nFeature" = "nFeature_RNA",
+  "percent_mt" = "percent.mt"
   )
-dev.off()
 
-## BY PATIENT
+# go over list of group names
+for (grp in names(umap_groups)) {
+  # select groups
+  orig_grp <- umap_groups[[grp]]
 
-pdf(file = "/scratch/svc_td_compbio/users/MaDeBa/figures/explore_seurat/rna_umap_1-27dim_06res_patient.pdf",
-    width=12,
-    height=9)
-DimPlot(seurat_obj_merged_joined,
-        reduction = "umap",
-        group.by = "patient",
-        label = FALSE,
-        raster.dpi = c(600,600)) +
-  labs(title = "RNA - patient (PCA dims = 1:27, res = 0.6)") +
-  theme(
-    plot.title = element_text(hjust=0.5),
-    legend.position = "bottom"
+  # create pdf with renamed group in name, change width if sample (because of high amount of samples)
+  if (grp == "sample") {
+    pdf_umap_width <- 18
+  } else {pdf_umap_width <- 12}
+  pdf(file = paste0(base_dir,subdir_umap,"/rna_umap_",grp,".pdf"),
+      width=pdf_umap_width,
+      height=9)
+  
+  # create DimPlot for categorical variables and featureplot for numerical
+  if (grp %in% c("cluster","patient","sample")) {
+    umap_p <- DimPlot(seurat_obj_merged_joined,
+                        reduction = "umap",
+                        group.by = orig_grp,
+                        label = FALSE,
+                        raster.dpi = c(600,600))
+  } else {
+      umap_p <- FeaturePlot(seurat_obj_merged_joined,
+                            reduction = "umap",
+                            features = orig_grp,
+                            cols = viridis(100))
+  }
+  
+  # apply styling and add title
+  umap_p <- umap_p +
+    labs(title = paste0("RNA - ",grp," (PCA dims = 1:27, res = 0.6)")) +
+    theme(
+      plot.title = element_text(hjust=0.5)
+      # ,legend.position = "bottom"
         )
-dev.off()
-
-## BY SAMPLE
-pdf(file = "/scratch/svc_td_compbio/users/MaDeBa/figures/explore_seurat/rna_umap_1-27dim_06res_sample.pdf",
-    width=18,
-    height=9)
-DimPlot(seurat_obj_merged_joined,
-        reduction = "umap",
-        group.by = "orig.ident",
-        label = FALSE,
-        raster.dpi = c(900,900)) +
-  labs(title = "RNA - sample (PCA dims = 1:27, res = 0.6)") +
-  theme(
-    plot.title = element_text(hjust=0.5),
-    legend.position = "right"
-  )
-dev.off()
-
-## BY ncount
-pdf(file = "/scratch/svc_td_compbio/users/MaDeBa/figures/explore_seurat/rna_umap_1-27dim_06res_ncount.pdf",
-    width=12,
-    height=9)
-FeaturePlot(seurat_obj_merged_joined,
-        reduction = "umap",
-        features = "nCount_RNA") +
-  # NoLegend() +
-  labs(title = "RNA - nCount (PCA dims = 1:27, res = 0.6)") +
-  theme(
-    plot.title = element_text(hjust=0.5)
-  )
-dev.off()
-
-## BY nfeature
-pdf(file = "/scratch/svc_td_compbio/users/MaDeBa/figures/explore_seurat/rna_umap_1-27dim_06res_nfeature.pdf",
-    width=12,
-    height=9)
-FeaturePlot(seurat_obj_merged_joined,
-            reduction = "umap",
-            features = "nFeature_RNA") +
-  # NoLegend() +
-  labs(title = "RNA - nFeature (PCA dims = 1:27, res = 0.6)") +
-  theme(
-    plot.title = element_text(hjust=0.5)
-  )
-dev.off()
-
-## BY percentmt
-pdf(file = "/scratch/svc_td_compbio/users/MaDeBa/figures/explore_seurat/rna_umap_1-27dim_06res_percentmt.pdf",
-    width=12,
-    height=9)
-FeaturePlot(seurat_obj_merged_joined,
-            reduction = "umap",
-            features = "percent.mt") +
-  # NoLegend() +
-  labs(title = "RNA - percent.mt (PCA dims = 1:27, res = 0.6)") +
-  theme(
-    plot.title = element_text(hjust=0.5)
-  )
-dev.off()
-
+  
+  # print plot and close pdf
+  print(umap_p)
+  dev.off()
+  # to check what has already been done: (prints in console)
+  print(grp)
+    }
 
 #Cluster composition per sample
 table(seurat_obj_merged_joined$seurat_clusters, seurat_obj_merged_joined$orig.ident)
@@ -359,34 +420,39 @@ markers <- list(
   Club = c("SCGB1A1", "SCGB3A1", "KRT7", "PIGR"),
   Mast = c("CPA3", "TPSAB1", "TPSB2", "KIT", "MS4A2"))
 
-# # visualize marker expression on umap
-# pdf(file = "/scratch/svc_td_compbio/users/MaDeBa/figures/explore_seurat/rna_umap_luminal.pdf", width=12, height=9)
-# FeaturePlot(
-#   seurat_obj_merged_joined,
-#   features = markers$Luminal
-# )
-# dev.off()
+# visualize marker expression on umap, use color viridis and improve for all celltypes
+subdir_ftplt <- "/figures/rna_marker_FeatPlot"
+for (ct in names(markers)) {
+  # select celltype markers for each celltype
+  ct_markers <- markers[[ct]]
+  
+  # create pdf with celltype in name
+  pdf(file = paste0(base_dir,subdir_ftplt,"/rna_umap_ftplt_",ct,".pdf"),
+      width=12,
+      height=9)
+  # create feature plot with colors of viridis, for each celltype
+  ft_plt <- FeaturePlot(   
+    seurat_obj_merged_joined,   
+    features = ct_markers, 
+    cols = viridis(100) 
+    )
+  
+  # add plot to pdf and close pdf
+  print(ft_plt)
+  dev.off()
+} 
 
-# use dotplot to show expression
-pdf(file = "/scratch/svc_td_compbio/users/MaDeBa/figures/explore_seurat/rna_markers_dotplot.pdf", width = 22, height = 10)
-DotPlot(seurat_obj_merged_joined, features = markers) + 
-  labs(title = "DotPlot - markers") +
-  theme(plot.title = element_text(hjust=0.5),
-        legend.position = "right")+
-  RotatedAxis()
-dev.off()
 
-# change colors and y-axis
+# use dotplot to show expression, change colors and y-axis
 pdf(file = "/scratch/svc_td_compbio/users/MaDeBa/figures/explore_seurat/rna_markers_dotplot_cluster.pdf", width = 22, height = 10)
 DotPlot(seurat_obj_merged_joined,
         features = markers,
         group.by = "seurat_clusters") + 
   scale_color_viridis_b() +
-  labs(title = "Marker expression - clusters ") +
+  labs(title = "Marker expression - clusters",
+       x = "Cell type - gene markers",
+       y = "Clusters") +
   theme(plot.title = element_text(hjust=0.5))+
   RotatedAxis()
 dev.off()
-
-# find markers
-# markers2 <- FindAllMarkers(seurat_obj_merged_joined)
 
